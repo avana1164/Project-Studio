092311c60f5a1d066db43ae3e2ae4eb0ed81e68d
AAnand05 <44912634+avana1164@users.noreply.github.com>
2023-11-08 01:48:04 -0500

    Update Project Studio.cpp
    The rotate_motor method will check if the lid is open or closed based on a boolean variable passed through the method.

--- Project Studio.cpp
+++ Project Studio.cpp
@@ -1,17 +1,26 @@
 #include <iostream>
 
 int main();
-void rotate_motor();
+void rotate_motor(int effort, bool open);
 int main(){
     bool quit = false; //if the user wants to stop using the device
     bool button_pressed = false; //if the button pressed, the pill dispenser lid will open
+    bool open = false;
     int heart_rate{}; //will track the heart rate
     int critical_value{}; //is a value that will be set and will determine if the heart rate is abnormal
     int effort{10}; //amount of effort for machine to open and close pill dispenser
     while(!quit){
         if(button_pressed){
-            rotate_motor();    
+            rotate_motor(effort, open);    
         }
     }
     return 0;
+}
+
+void rotate_motor(int effort, bool open){
+    if(open){
+        //open lid with given effort value
+    } else {
+        //close lid with given effort value
+    }
 }
