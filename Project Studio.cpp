150b429fc7be21c9c966e4e7b3981caec269f15f
AAnand05 <44912634+avana1164@users.noreply.github.com>
2023-11-08 01:33:48 -0500

    Add files via upload
    Initialize variables that will store critical information for how the microcontroller should send signals.

---
+++ Project Studio.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+int main();
+
+int main(){
+    bool quit = false; //if the user wants to stop using the device
+    bool button_pressed = false; //if the button pressed, the pill dispenser lid will open
+    int heart_rate{}; //will track the heart rate
+    int critical_value{}; //is a value that will be set and will determine if the heart rate is abnormal
+    int effort{10}; //amount of effort for machine to open and close pill dispenser
+    return 0;
+}
