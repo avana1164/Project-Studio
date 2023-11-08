c86324d9a015a924a5a310f0f10215e448a2c703
AAnand05 <44912634+avana1164@users.noreply.github.com>
2023-11-08 01:53:22 -0500

    Update Project Studio.cpp
    The second if statement will consider if the user is in a critical condition and they need to take a pill, which in that case will make an LED glow. The rotate_motor() method will now toggle between an opened and closed state.

--- Project Studio.cpp
+++ Project Studio.cpp
@@ -7,20 +7,26 @@ int main(){
     bool button_pressed = false; //if the button pressed, the pill dispenser lid will open
     bool open = false;
     int heart_rate{}; //will track the heart rate
-    int critical_value{}; //is a value that will be set and will determine if the heart rate is abnormal
+    int critical_value{150}; //is a value that will be set and will determine if the heart rate is abnormal
     int effort{10}; //amount of effort for machine to open and close pill dispenser
     while(!quit){
         if(button_pressed){
-            rotate_motor(effort, open);    
+            rotate_motor(effort, open); 
+        } 
+
+        if(heart_rate > 100 || heart_rate < 50 || heart_rate == critical_value){
+            //make LED glow
         }
     }
     return 0;
 }
 
-void rotate_motor(int effort, bool open){
+void rotate_motor(int effort, bool &open){
     if(open){
-        //open lid with given effort value
-    } else {
         //close lid with given effort value
+        open = false;
+    } else {
+        //open lid with given effort value
+        open = true;
     }
 }
