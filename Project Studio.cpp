d7264d60f57e75f2436c272af934e7c4d2677fea
AAnand05 <44912634+avana1164@users.noreply.github.com>
2023-11-08 02:01:53 -0500

    Update Project Studio.cpp
    User interface implemented to allow the user to make a decision to press the button. A randomized heart rate is being considered to simulate how a real life heart functions.

--- Project Studio.cpp
+++ Project Studio.cpp
@@ -9,14 +9,21 @@ int main(){
     int heart_rate{}; //will track the heart rate
     int critical_value{150}; //is a value that will be set and will determine if the heart rate is abnormal
     int effort{10}; //amount of effort for machine to open and close pill dispenser
+    char user_input{};
     while(!quit){
-        if(button_pressed){
+        std::cout << "Do you want to open the button? (type \'Y\' for yes)" << std::endl;
+        std::cin >> user_input;
+        if(user_input == 'Y'){
             rotate_motor(effort, open); 
         } 
 
         if(heart_rate > 100 || heart_rate < 50 || heart_rate == critical_value){
             //make LED glow
         }
+
+        std::cout << heart_rate << std::endl;
+        //randomize heart rate between 60 and 70 every iteration
+        //occassionally make heart rate abnormal
     }
     return 0;
 }
