d6aa18d6a8545fe0d050e612b98b5bca17795d08
AAnand05 <44912634+avana1164@users.noreply.github.com>
2023-11-08 01:43:02 -0500

    Update Project Studio.cpp
    While loop will go through all the conditions and will send signals from the microcontroller. rotate_motor() method will signal the motor to move.

--- Project Studio.cpp
+++ Project Studio.cpp
@@ -1,12 +1,17 @@
 #include <iostream>
 
 int main();
-
+void rotate_motor();
 int main(){
     bool quit = false; //if the user wants to stop using the device
     bool button_pressed = false; //if the button pressed, the pill dispenser lid will open
     int heart_rate{}; //will track the heart rate
     int critical_value{}; //is a value that will be set and will determine if the heart rate is abnormal
     int effort{10}; //amount of effort for machine to open and close pill dispenser
+    while(!quit){
+        if(button_pressed){
+            rotate_motor();    
+        }
+    }
     return 0;
 }
